c338bd69a7aa77e197b89d7e3a5046ef00c1f0c6
jingwang-web <15155572523@163.com>
2022-07-15 16:58:35 +0800

    fix(bug):注释掉代理配置

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,13 @@ module.exports = {
         host: 'localhost',
         port: '8080',
         https: false,
-        proxy: {
-            '/proxyApi': {
-                target: 'https://114.132.223.235:8082/',
-                pathRewrite: {'^/proxyApi': ''}, // 下面这个配置的作用是, 把`/api/api地址`转发给服务器的只有`api地址`, 去掉`/api`
-                // ws: true, //  websocket 用于支持websocket,不写也默认true
-                changeOrigin: true, //不写也默认true,用于控制请求头中的host值
-            }
-        }
+        // proxy: {
+        //     '/proxyApi': {
+        //         target: 'https://114.132.223.235:8082/',
+        //         pathRewrite: {'^/proxyApi': ''}, // 下面这个配置的作用是, 把`/api/api地址`转发给服务器的只有`api地址`, 去掉`/api`
+        //         // ws: true, //  websocket 用于支持websocket,不写也默认true
+        //         changeOrigin: true, //不写也默认true,用于控制请求头中的host值
+        //     }
+        // }
     }
 }
